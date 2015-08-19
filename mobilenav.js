4c439ebf4051d34e83c6efc7755b80f4dfc461c0
Jonathan Vickers <jrvickers86@gmail.com>
2015-08-19 16:07:32 +0100

    Updated package to use a single push transformation instead of multiple

--- mobilenav.js
+++ mobilenav.js
@@ -1,5 +1,21 @@
 import * as dom from '../../../lib/components/js.dom/dom';
 import * as helpers from '../../../lib/components/js.helpers/helpers';
+
+/**
+
+Current issues
+
+- Fixed elements whos parent has a transform appear fixed to the parent rather than the viewport.  See: http://stackoverflow.com/questions/15194313/webkit-css-transform3d-position-fixed-issue
+	- As a work around the .page element (transformed element) height is altered to 100% the viewport.  You are still able to scroll within the menu itself (scroll bars will appear on desktop, not mobile).  This means that the page is jumped to the top when menus are opened.
+
+- Top and bottom push menus gain an incorrect height when using a percentage based height.
+	- The same work around above can fix this issue.
+
+**/
+
+
+
+
 /**
  * Creates a new mobile nav instance
  * @class MobileNav
@@ -13,14 +29,13 @@ class MobileNav {
 	constructor(navElement) {
 		
 		this.element = navElement;
-		
-		this.ready = false;
 
 		this.type = this.element.getAttribute('data-reveal-type');
 		this.position  = this.element.getAttribute('data-position');
+
 		this.string = this.type + '-' + this.position;
-		this.pageBody = document.getElementsByClassName('page__body')[0];
 		this.page = document.getElementsByClassName('page')[0];
+
 		this.toggleElements = document.getElementsByClassName('page__nav-show');
 		
 		this.setClass()
@@ -32,16 +47,17 @@ class MobileNav {
      * @memberOf MobileNav
     */
 	show() {
-		if (!this.ready){
-			this.ready = true;
-			dom.addClass(this.element, 'page__nav--ready');
-		}		
 
-		if (this.type == 'push') {
-			this.setBodyTransform();
-		}
+		dom.addClass(this.element, 'page__nav--ready');
+
+			dom.addClass(document.body, 'restrict-height');
+	
 		
-		dom.addClass(this.page, 'show-menu' + '--' + this.string);
+		
+		/* the setTimeout is needed to make sure .page height is 100% viewport when the menu is shown. */
+		setTimeout(() => {
+			dom.addClass(this.page, 'show-menu' + '--' + this.string);
+		},25)		
 
 		return this;
 	}
@@ -50,8 +66,17 @@ class MobileNav {
      * @memberOf MobileNav
     */
 	hide() {	
-		this.resetBodyTransform();
+		
+		let dur = helpers.getTransitionDuration(window.getComputedStyle(this.page));
+		
 		dom.removeClass(this.page, 'show-menu' + '--' + this.string);
+
+		/* remove .page height restiction */
+		setTimeout(() => {
+			dom.removeClass(this.element, 'page__nav--ready');
+			dom.removeClass(document.body, 'restrict-height')
+		}, dur)
+
 		return this;
 	}
 	/**
@@ -114,22 +139,7 @@ class MobileNav {
 		return this;
 	}
 	
-	/**
-     * Sets the page body transform
-     * @memberOf MobileNav
-    */
-	setBodyTransform() {
-		helpers.setTransformValue(this.pageBody.style, this.transform_value);
-		return this;
-	}
-	/**
-     * Removes the page body transform
-     * @memberOf MobileNav
-    */
-	resetBodyTransform() {
-		helpers.setTransformValue(this.pageBody.style, 'translateX(0px) translateY(0px)');
-		return this;
-	}
+
 
 }
 
